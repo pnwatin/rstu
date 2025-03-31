7747a36fc57adf1e50bdbe11ccc844a49a6c378c
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2025-03-31 21:36:37 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+pub trait Sorter {
+    fn sort<T>(slice: &mut [T])
+    where
+        T: std::cmp::Ord;
+}
+
+pub struct StdSorter;
+
+impl Sorter for StdSorter {
+    fn sort<T>(slice: &mut [T])
+    where
+        T: std::cmp::Ord,
+    {
+        slice.sort();
+    }
+}
+
+pub struct StdUnstableSorter;
+
+impl Sorter for StdUnstableSorter {
+    fn sort<T>(slice: &mut [T])
+    where
+        T: std::cmp::Ord,
+    {
+        slice.sort_unstable();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quickcheck::quickcheck;
+
+    quickcheck! {
+        fn std_works(slice: Vec<u32>) -> bool {
+            let mut slice =  slice;
+            StdSorter::sort(&mut slice);
+
+            slice.is_sorted()
+        }
+    }
+
+    quickcheck! {
+        fn std_unstable_works(slice: Vec<u32>) -> bool {
+            let mut slice =  slice;
+            StdUnstableSorter::sort(&mut slice);
+
+            slice.is_sorted()
+        }
+    }
+}
